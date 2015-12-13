e3b2586b263d7c0b4af5a525aa53d52634ea55c9
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-13 18:01:22 +0200

    My first test for Bizz

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FizzBuzz
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzz
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual("Fizz", CalculateFizzBuzz(3));
+        }
+
+        String CalculateFizzBuzz(int myNumber)
+        {
+            if (myNumber % 3 == 0)
+                return "Fizz";
+            return Convert.ToString(myNumber);
         }
     }
 }
