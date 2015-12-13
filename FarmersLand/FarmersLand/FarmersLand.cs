920c1392a29fba8923c6c079e29892fadfd4a85a
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-13 19:19:35 +0200

    My test for Farmer's Land, with our result for initial dimensions

--- FarmersLand/FarmersLand/FarmersLand.cs
+++ FarmersLand/FarmersLand/FarmersLand.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FarmersLand
 {
     [TestClass]
-    public class UnitTest1
+    public class FarmersLand
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForInitialDimension()
         {
+            Assert.AreEqual(770, CalculateInitialDimension(770000));
+        }
+
+        double CalculateInitialDimension(double finalDimension)
+        {
+            double a = 1;
+            double b = 230;
+            double x1 = 0;
+
+            double equation = a * a + 230 * b - finalDimension;
+            double sqrtpart = (b * b) - (4 * a * (-1)*finalDimension);
+            if (sqrtpart > 0)
+            {
+                x1 = (-b + Math.Sqrt(sqrtpart)) / (a * 2);
+                // x2 = (-b - Math.Sqrt(sqrtpart)) / (2 * a); we don't need this because gives us a negative value
+            }
+           
+            return x1;
         }
     }
 }
