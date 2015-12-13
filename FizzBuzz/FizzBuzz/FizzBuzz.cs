b6a5c9aefa59319dfe1a6456313f6a03548845ee
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-13 18:07:30 +0200

    My third test for FizzBuzz

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -11,18 +11,29 @@ namespace FizzBuzz
         {
             Assert.AreEqual("Fizz", CalculateFizzBuzz(3));
         }
+
         [TestMethod]
         public void TestBuzz()
         {
             Assert.AreEqual("Buzz", CalculateFizzBuzz(5));
         }
 
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", CalculateFizzBuzz(30));
+        }
+
         String CalculateFizzBuzz(int myNumber)
         {
+            if (myNumber % 5 == 0 && myNumber % 3 == 0)
+                return "FizzBuzz";
             if (myNumber % 3 == 0)
                 return "Fizz";
             if (myNumber % 5 == 0)
                 return "Buzz";
+            if ((myNumber % 5 == 0) && (myNumber % 3 == 0))
+                return "FizzBuzz";
             return Convert.ToString(myNumber);
         }
     }
