4ebfcf0e896133491145b460b7f28f02645617fa
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-13 18:22:54 +0200

    My test for parity to verify if they can split the watermallon

--- Watermallon/Watermallon/Watermallon.cs
+++ Watermallon/Watermallon/Watermallon.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Watermallon
 {
     [TestClass]
-    public class UnitTest1
+    public class Watermallon
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual("Da", CalculateParityForWatermallon(2));
+        }
+
+        string CalculateParityForWatermallon(int myNumberParity)
+        {
+            if (myNumberParity % 2 == 0)
+                return "Da";
+            return Convert.ToString(myNumberParity);
         }
     }
 }
