726310584e844fc31d1f9b43fa73f50a9af9ff31
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-13 19:30:05 +0200

    My test for an even number which does not decompose in two even numbers

--- Watermallon/Watermallon/Watermallon.cs
+++ Watermallon/Watermallon/Watermallon.cs
@@ -11,15 +11,22 @@ namespace Watermallon
         {
             Assert.AreEqual("Da", CalculateParityForWatermallon(4));
         }
+
         [TestMethod]
         public void TestForParityWithResponseNo()
         {
             Assert.AreEqual("Nu", CalculateParityForWatermallon(3));
         }
 
+        [TestMethod]
+        public void TestForNumberTwo()
+        {
+            Assert.AreEqual("Nu", CalculateParityForWatermallon(2));
+        }
+
         string CalculateParityForWatermallon(int myNumberParity)
         {
-            bool testParity = myNumberParity % 2 == 0;
+            bool testParity = (myNumberParity != 2 && myNumberParity % 2 == 0);
             if (testParity)
                 return "Da";
             else
