c476af267095c371fdef1650dd2a11ab6116d8c9
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-18 01:37:07 +0200

    My second good test for medium period

--- Rent/Rent/Rent.cs
+++ Rent/Rent/Rent.cs
@@ -12,9 +12,22 @@ namespace Rent
             Assert.AreEqual(2, CalculateRentPenality(100, 1));
         }
 
+        [TestMethod]
+        public void RentPenalityForMediumPeriod()
+        {
+            Assert.AreEqual(55, CalculateRentPenality(100, 11));
+        }
+
         double CalculateRentPenality(int rent, int days)
         {
-            return 0.02 * rent * days;
+            double RentForShorPeriod = 0.02 * rent * days;
+            if (days >= 1 && days <= 10)
+            {
+                return RentForShorPeriod;
+            }
+            else
+            return 0.05 * rent * days;
+
         }
     }
 }
