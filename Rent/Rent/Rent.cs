f5cbe74c1398c8cf898cd83de1f4ee206fce6818
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-18 01:52:20 +0200

    My fourth good test and final version of problem

--- Rent/Rent/Rent.cs
+++ Rent/Rent/Rent.cs
@@ -24,10 +24,17 @@ namespace Rent
             Assert.AreEqual(310, CalculateRentPenality(100, 31));
         }
 
+        [TestMethod]
+        public void RentPenalityForLongPeriod2()
+        {
+            Assert.AreEqual(0, CalculateRentPenality(100, 45));
+        }
+
         double CalculateRentPenality(int rent, int days)
         {
             double RentForShortPeriod = 0.02 * rent * days;
             double RentForMediumPeriod = 0.05 * rent * days;
+            double RentForLongPeriod = 0.1 * rent * days;
 
             if (days >= 1 && days <= 10)
             {
@@ -37,9 +44,12 @@ namespace Rent
             {
                 return RentForMediumPeriod;
             }
+            if (days >= 31 && days <= 40)
+            {
+                return RentForLongPeriod;
+            }
             else
-                return 0.1 * rent * days;
-
+                return 0; // for a number of days, grater than 41, return 0;
         }
     }
 }
