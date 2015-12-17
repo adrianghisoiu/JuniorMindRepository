a56a5c5afff78f24ceaad7bd630213ce4f4197c3
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-18 01:08:21 +0200

    My last good test for converting numbers in Roman Numbers

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -18,12 +18,18 @@ namespace RomanNumbers
             Assert.AreEqual("XXVI", ConvertInRomanNumbers(26));
         }
 
+        [TestMethod]
+        public void ConversionOfSixHundredSixtySix()
+        {
+            Assert.AreEqual("DCLXVI", ConvertInRomanNumbers(666));
+        }
+
         string ConvertInRomanNumbers(int myNumber)
         {
             string[] romanNumbersOne = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-            string[] romanNumberTwo = new string[] { "", "X", "XX", "XXX", "LX", "L", "LX", "LXX", "LXXX", "XC" };
-
-            return romanNumberTwo[myNumber / 10] + romanNumbersOne[myNumber % 10];
+            string[] romanNumberTwo = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] romanNumberThree = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+                return romanNumberThree[myNumber/100] + romanNumberTwo[myNumber % 100 / 10] + romanNumbersOne[myNumber % 10];
         }
     }
 }
