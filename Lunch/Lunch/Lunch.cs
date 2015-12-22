1bb428170b0c72a1a9bf43f56679b69fad1da62e
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-22 12:32:10 +0200

    My fourth test with a wrong condition

--- Lunch/Lunch/Lunch.cs
+++ Lunch/Lunch/Lunch.cs
@@ -18,17 +18,25 @@ namespace Lunch
             Assert.AreEqual("You don't meet your friend", CalculateIfWeMeetAgain(20));
         }
 
+        [TestMethod]
+        public void TestWithWrongCondition()
+        {
+            Assert.AreEqual("Wrong condition", CalculateIfWeMeetAgain(-20));
+        }
+
         string CalculateIfWeMeetAgain(int numberOfDays)
         {
+            bool myCondition = (numberOfDays % 4) == 0 && (numberOfDays % 6 == 0);
+
             while (numberOfDays > 0)
             {
-                if ((numberOfDays % 4) == 0 && (numberOfDays % 6 == 0))
+                if (myCondition)
                     return "You meet your friend again";
                 else
                     return "You don't meet your friend";
             }
 
-            return Convert.ToString(numberOfDays);
+            return "Wrong condition";
         }
     }
 }
