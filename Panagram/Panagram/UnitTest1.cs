7599f918b412f84472241f898f223e4c04bfc86c
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-30 14:00:15 +0200

    My first good test for Panagram

--- Panagram/Panagram/UnitTest1.cs
+++ Panagram/Panagram/UnitTest1.cs
@@ -14,7 +14,20 @@ namespace Panagram
 
         string SeeIfItsAPanagrams(string myString)
         {
-            return "Yes";
+            int contor = 1;
+            bool check = true;
+            for (int i = 0; i < (myString.Length-1); i++)
+            {
+                for (int j = i + 1; j < myString.Length; j++)
+                    if (myString[i] == myString[j])
+                        check = false;
+                if (check == true)
+                    contor++;
+            }
+            if (contor == 26)
+                return "Yes";
+            else
+                return "No";
         }
     }
 }
