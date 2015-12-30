08fad69d5d4496c9f617005a513e49b55524d4ba
adrianghisoiu <ghisoiuiadrian@gmail.com>
2015-12-30 15:34:05 +0200

    I renamed my UnitTest1 in Panagram

--- Panagram/Panagram/Panagram.cs
+++ Panagram/Panagram/Panagram.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace Panagram
 {
     [TestClass]
-    public class UnitTest1
+    public class Panagram
     {
         [TestMethod]
         public void TestForPanagram()
