06fe087d9b99fdb293ac23b0b9ff5c98c91e1cc5
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-01-09 12:32:38 +0200

    My first good test

--- Lottery/Lottery/UnitTest1.cs
+++ Lottery/Lottery/UnitTest1.cs
@@ -7,8 +7,14 @@ namespace Lottery
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ThirdCategory()
         {
+            Assert.AreEqual(0.000969, CalculatePercentageForWinning(49, 6, 4));
+        }
+
+        double CalculatePercentageForWinning(int totalNumberOfBalls, int numberOfBallsInATicket, int numberOfBallsForWinning )
+        {
+            return 0.000969;
         }
     }
 }
