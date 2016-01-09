3ead1af1e5e4aa633b2b7c34a1f2bf7920350d27
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-01-09 13:18:14 +0200

    My test for combinations

--- Lottery/Lottery/UnitTest1.cs
+++ Lottery/Lottery/UnitTest1.cs
@@ -6,15 +6,36 @@ namespace Lottery
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        /*[TestMethod]
         public void ThirdCategory()
         {
-            Assert.AreEqual(0.000969, CalculatePercentageForWinning(49, 6, 4));
+            Assert.AreEqual(0.000969, CalculatePercentageForWinning(49, 6,4));
+        }*/
+
+        [TestMethod]
+        public void TestForCombinationsFunction()
+        {
+            Assert.AreEqual(15, CalculateCombinations(6, 4));
         }
 
-        double CalculatePercentageForWinning(int totalNumberOfBalls, int numberOfBallsInATicket, int numberOfBallsForWinning )
+        /*  double CalculatePercentageForWinning(int totalNumberOfBalls, int numberOfBallsInATicket, int numberOfBallsForWinning )
+          {
+              int remainingBalls = totalNumberOfBalls - numberOfBallsInATicket;
+              int balls = numberOfBallsInATicket - numberOfBallsForWinning;
+
+          }*/
+
+        double CalculateCombinations(int n, int k)
         {
-            return 0.000969;
+            return Factorial(n) / (Factorial(k) * Factorial(n - k));
+        }
+
+        private static double Factorial(int n)
+        {
+            double total = 1;
+            for (int i = 1; i <= n; i++)
+                total = total * i;
+            return total;
         }
     }
 }
