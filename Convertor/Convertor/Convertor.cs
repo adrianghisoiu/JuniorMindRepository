6b27510d90f9e8e45a1f7b01109be7006068ec4c
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-02-25 17:53:58 +0200

    My good test for GetElement

--- Convertor/Convertor/Convertor.cs
+++ Convertor/Convertor/Convertor.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Convertor
 {
     [TestClass]
-    public class UnitTest1
+    public class Convertor
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetElement()
         {
+            Assert.AreEqual(1, GetElement(new byte[] { 3, 2, 1}, 0));
+        }
+
+        byte GetElement(byte[] myByteArray, int position)
+        {
+            return myByteArray[myByteArray.Length - 1 - position];
         }
     }
 }
