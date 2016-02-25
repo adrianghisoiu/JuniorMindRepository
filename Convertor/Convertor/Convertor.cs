1b391e8ed639d0a4a8d01c428e193bacf541d42d
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-02-25 18:52:13 +0200

    First good test for ConvertToBinary

--- Convertor/Convertor/Convertor.cs
+++ Convertor/Convertor/Convertor.cs
@@ -13,9 +13,29 @@ namespace Convertor
             Assert.AreEqual(0, GetElement(new byte[] { 3, 2, 1 }, 4));
         }
 
+        [TestMethod]
+        public void TestForConvert()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0}, ConvertToBinary(2));
+        }
+
         byte GetElement(byte[] myByteArray, int position)
         {
             return (byte)(position < myByteArray.Length ? myByteArray[myByteArray.Length - 1 - position] : 0);
+        }
+
+        byte[] ConvertToBinary(int numberToConvert)
+        {
+            int i = 0;
+            byte[] convertedNumber = new byte[0];
+            while(numberToConvert > 0)
+            {
+                Array.Resize(ref convertedNumber, i+1);
+                convertedNumber[i++] = (byte)(numberToConvert % 2);
+                numberToConvert = numberToConvert / 2;
+            }
+            Array.Reverse(convertedNumber);
+            return convertedNumber;
         }
     }
 }
