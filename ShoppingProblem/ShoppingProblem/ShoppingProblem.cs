5ecec0b0cb136fd311604129f9a2c90716290113
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-10 20:10:21 +0200

    First test for TotalPrice

--- ShoppingProblem/ShoppingProblem/ShoppingProblem.cs
+++ ShoppingProblem/ShoppingProblem/ShoppingProblem.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ShoppingProblem
 {
     [TestClass]
-    public class UnitTest1
+    public class ShoppingProblem
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForTotalPrice()
         {
+            var product = new Products[] { new Products(10, "Corn"), new Products(1010, "Samsung Smart TV"), new Products(3.5 , "Bread") };
+            Assert.AreEqual(1023.5, CalculateTotalPrice(product));
+        }
+
+        struct Products
+        {
+            public double price;
+            public string productName;
+
+            public Products(double price, string productName)
+            {
+                this.price = price;
+                this.productName = productName;
+            }               
+        }
+
+        double CalculateTotalPrice(Products[] product)
+        {
+            double totalPrice = 0;
+            for (int i = 0; i < product.Length; i++)
+                totalPrice += product[i].price;
+            return totalPrice;
         }
     }
 }
