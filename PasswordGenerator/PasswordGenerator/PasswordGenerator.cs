dbc1e2bc4cf015e1c8dbaa3720f875b659d9e202
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-15 18:23:45 +0200

    Test for SeeIfItsSmallLetter and Test for SmallLetters

---
+++ PasswordGenerator/PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordGenerator
+{
+    [TestClass]
+    public class PasswordGenerator
+    {
+        [TestMethod]
+        public void TestForSmallLetters()
+        {
+            Assert.IsTrue(SeeIfSmallLetters(SmallLettersGenerator(6)));
+        }
+
+        [TestMethod]
+        public void TestToSeeIfItsSmall()
+        {
+            Assert.IsTrue(SeeIfSmallLetters("abc"));
+        }
+
+        string SmallLettersGenerator(int number=6)
+        {
+            Random rand = new Random();
+            char c = (char)0;
+            string myString = null;
+            for (int i = 0; i <= number; i++)
+            {
+                c = (char)('a' + rand.Next(0, 26));
+                myString += c.ToString();
+            }
+            return myString;
+        }
+
+        bool SeeIfSmallLetters(string myString)
+        {
+            bool check = true;
+            for (int i = 0; i < myString.Length; i++)
+                if(myString[i] < 'a' && myString[i] > 'z')
+                {
+                    check = false;
+                    break;
+                }
+            return check;
+        }
+    }
+}
