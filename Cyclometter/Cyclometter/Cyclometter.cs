cf10947d73c85f09c49c7ff8152dfb3b04decfa2
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-22 22:31:32 +0200

    Calculate distance for one Cyclist

--- Cyclometter/Cyclometter/Cyclometter.cs
+++ Cyclometter/Cyclometter/Cyclometter.cs
@@ -4,11 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Cyclometter
 {
     [TestClass]
-    public class UnitTest1
+    public class Cyclometter
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FindOneCyclistDistance()
         {
+            var cyclist = new Cyclist[] { new Cyclist("George", new int[] { 5, 6, 4, 1 }, 6) };
+            Assert.AreEqual(301.59, CalculateDistance(cyclist[0]), 1e-2);
+        }
+
+        struct Cyclist
+        {
+            public string nameOfCyclist;
+            public int[] noOfRotations;
+            public int diameter;
+
+            public Cyclist(string nameOfCyclist, int[] noOfRotations, int diameter)
+            {
+                this.nameOfCyclist = nameOfCyclist;
+                this.noOfRotations = noOfRotations;
+                this.diameter = diameter;
+            }
+        }
+
+        double CalculateDistance(Cyclist oneCyclist)
+        {
+            double circumference = Math.PI * oneCyclist.diameter;
+            double distance = 0;
+            for (int i = 0; i < oneCyclist.noOfRotations.Length; i++)
+                distance += circumference * oneCyclist.noOfRotations[i];
+
+            return distance;
         }
     }
 }
