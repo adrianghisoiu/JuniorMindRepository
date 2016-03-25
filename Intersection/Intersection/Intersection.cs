4002f45cceb34ba18a7dc9d79db77b6d067a2397
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-25 23:36:15 +0200

    Second good test for intersection point

--- Intersection/Intersection/Intersection.cs
+++ Intersection/Intersection/Intersection.cs
@@ -18,7 +18,9 @@ namespace Intersection
         public void TestForIntersectionPoint()
         {
             var directions = new Directions[] { Directions.Up, Directions.Down, Directions.Left, Directions.Right };
+            var secondDirections = new Directions[] { Directions.Up, Directions.Up };
             Assert.AreEqual(true, FindIfCoordinatesIntersect(new Coordonates(2, 1), directions));
+            Assert.AreEqual(false, FindIfCoordinatesIntersect(new Coordonates(3, 1), secondDirections));
         }
 
         enum Directions
@@ -58,15 +60,15 @@ namespace Intersection
         bool FindIfCoordinatesIntersect(Coordonates startCoordonates, Directions[] directions)
         {
             Coordonates[] newCoordonates = new Coordonates[directions.Length + 1];
-            newCoordonates[0] = startCoordonates;
             Coordonates coordonates = new Coordonates();
-            for (int i = 1; i <= directions.Length; i++)
+            for (int i = 0; i < directions.Length; i++)
             {
-                coordonates = CalculateNewCoordonates(directions[i], newCoordonates[i - 1]);
-                if (Array.IndexOf(newCoordonates, newCoordonates[i]) >= 0)
+                coordonates = CalculateNewCoordonates(directions[i], startCoordonates);
+                if (Array.IndexOf(newCoordonates, coordonates) >= 0)
                     return true;
                 else
                     newCoordonates[i] = coordonates;
+            startCoordonates = coordonates;
             }
             return false;
         }
