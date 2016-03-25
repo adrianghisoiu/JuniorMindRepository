93826bd66464a329420aabeb04f16217cea98955
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-25 23:27:26 +0200

    Good test for intersection point

--- Intersection/Intersection/Intersection.cs
+++ Intersection/Intersection/Intersection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Intersection
 {
@@ -11,6 +12,13 @@ namespace Intersection
         {
             var point = new Coordonates(2, 2);
             Assert.AreEqual(point, CalculateNewCoordonates(Directions.Up, new Coordonates(2, 1)));
+        }
+
+        [TestMethod]
+        public void TestForIntersectionPoint()
+        {
+            var directions = new Directions[] { Directions.Up, Directions.Down, Directions.Left, Directions.Right };
+            Assert.AreEqual(true, FindIfCoordinatesIntersect(new Coordonates(2, 1), directions));
         }
 
         enum Directions
@@ -33,18 +41,34 @@ namespace Intersection
             }
         }
 
-        Coordonates CalculateNewCoordonates(Directions directions, Coordonates startCoordonates)
+        Coordonates CalculateNewCoordonates(Directions direction, Coordonates startCoordonates)
         {
-            if (directions.Equals(Directions.Up))
+            if (direction.Equals(Directions.Up))
                 startCoordonates.y++;
-            if (directions.Equals(Directions.Down))
+            if (direction.Equals(Directions.Down))
                 startCoordonates.y--;
-            if (directions.Equals(Directions.Left))
+            if (direction.Equals(Directions.Left))
                 startCoordonates.x--;
-            if (directions.Equals(Directions.Right))
+            if (direction.Equals(Directions.Right))
                 startCoordonates.x++;
 
             return startCoordonates;
+        }
+
+        bool FindIfCoordinatesIntersect(Coordonates startCoordonates, Directions[] directions)
+        {
+            Coordonates[] newCoordonates = new Coordonates[directions.Length + 1];
+            newCoordonates[0] = startCoordonates;
+            Coordonates coordonates = new Coordonates();
+            for (int i = 1; i <= directions.Length; i++)
+            {
+                coordonates = CalculateNewCoordonates(directions[i], newCoordonates[i - 1]);
+                if (Array.IndexOf(newCoordonates, newCoordonates[i]) >= 0)
+                    return true;
+                else
+                    newCoordonates[i] = coordonates;
+            }
+            return false;
         }
     }
 }
