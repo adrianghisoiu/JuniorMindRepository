cae026859c504251ba56b937d771667c95d5ad39
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-25 22:51:00 +0200

    Test for CalculateNewCoordonates

--- Intersection/Intersection/Intersection.cs
+++ Intersection/Intersection/Intersection.cs
@@ -4,11 +4,47 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Intersection
 {
     [TestClass]
-    public class UnitTest1
+    public class Intersection
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForCalculateNewCoordonates()
         {
+            var point = new Coordonates(2, 2);
+            Assert.AreEqual(point, CalculateNewCoordonates(Directions.Up, new Coordonates(2, 1)));
+        }
+
+        enum Directions
+        {
+            Up = 1,
+            Down,
+            Left,
+            Right
+        }
+
+        struct Coordonates
+        {
+            public int x;
+            public int y;
+
+            public Coordonates(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        Coordonates CalculateNewCoordonates(Directions directions, Coordonates startCoordonates)
+        {
+            if (directions.Equals(Directions.Up))
+                startCoordonates.y++;
+            if (directions.Equals(Directions.Down))
+                startCoordonates.y--;
+            if (directions.Equals(Directions.Left))
+                startCoordonates.x--;
+            if (directions.Equals(Directions.Right))
+                startCoordonates.x++;
+
+            return startCoordonates;
         }
     }
 }
