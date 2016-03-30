979e5bac2a9cfe091b5b8bb388ae8f9bed81090c
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-03-30 14:42:43 +0300

    Second good test for Fibonacci

--- Fibonacci/Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -12,10 +12,18 @@ namespace Fibonacci
             Assert.AreEqual(1, FindTheFibonacciNumber(1));
         }
 
+        [TestMethod]
+        public void FindTheSeventhFibonacciNumber()
+        {
+            Assert.AreEqual(13, FindTheFibonacciNumber(7));
+        }
+
     
         int FindTheFibonacciNumber(int number)
         {
-            return number;
+            if (number < 2)
+                return number;
+            return FindTheFibonacciNumber(number - 1) + FindTheFibonacciNumber(number - 2);
         }
     }
 }
