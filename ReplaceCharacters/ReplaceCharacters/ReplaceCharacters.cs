bcfff2f5c8219f7480fed86569a3e17450495c47
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-07 13:15:53 +0300

    Final version of ReplaceCharacters

--- ReplaceCharacters/ReplaceCharacters/ReplaceCharacters.cs
+++ ReplaceCharacters/ReplaceCharacters/ReplaceCharacters.cs
@@ -15,10 +15,7 @@ namespace ReplaceCharacters
         string ReplaceChar(string firstString, char toReplace, string secondString)
         {
             if (firstString.Length > 0)
-                if (firstString[firstString.Length - 1] == toReplace)
-                    return ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + secondString;
-            else
-            return ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + firstString[firstString.Length - 1];
+            return (firstString[firstString.Length - 1] == toReplace) ? ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + secondString : ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + firstString[firstString.Length - 1];
 
             return firstString;
         }
