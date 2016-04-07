f232e45bf2832399eb2f2bdf4a3bd6d21f21530d
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-07 13:14:44 +0300

    Good test for char replace

--- ReplaceCharacters/ReplaceCharacters/ReplaceCharacters.cs
+++ ReplaceCharacters/ReplaceCharacters/ReplaceCharacters.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ReplaceCharacters
 {
     [TestClass]
-    public class UnitTest1
+    public class ReplaceCharacters
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CharToReplace()
         {
+            Assert.AreEqual("ahhchh", ReplaceChar("abcb", 'b', "hh"));
+        }
+
+        string ReplaceChar(string firstString, char toReplace, string secondString)
+        {
+            if (firstString.Length > 0)
+                if (firstString[firstString.Length - 1] == toReplace)
+                    return ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + secondString;
+            else
+            return ReplaceChar(firstString.Substring(0, firstString.Length - 1), toReplace, secondString) + firstString[firstString.Length - 1];
+
+            return firstString;
         }
     }
 }
