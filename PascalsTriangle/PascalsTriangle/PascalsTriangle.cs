72afd48a7e75701f8d6b1fcdef7f1fbf0a96ef3b
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-10 13:56:54 +0300

    Calculate Pascal of three

--- PascalsTriangle/PascalsTriangle/PascalsTriangle.cs
+++ PascalsTriangle/PascalsTriangle/PascalsTriangle.cs
@@ -11,13 +11,24 @@ namespace PascalsTriangle
         {
             CollectionAssert.AreEqual(new int[] { 1 }, CalculatePascal(1));
             CollectionAssert.AreEqual(new int[] { 1, 1 }, CalculatePascal(2));
+            CollectionAssert.AreEqual(new int[] { 1, 2, 1 }, CalculatePascal(3));
         }
 
         int[] CalculatePascal(int line)
         {
+            int[] final = new int[line];
             int[] row = new int[line];
+            if (line == 1)
+            {
+                return new int[] { 1 };
+            }
+
             row[0] = 1;
             row[line - 1] = 1;
+            final = CalculatePascal(line - 1);
+                for (int j = 1; j < line - 1; j++)
+                    row[j] = final[j - 1] + final[j];
+            
             return row;
         }
     }
