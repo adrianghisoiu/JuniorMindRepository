23316881d353797c7d894f736d775cbb6a309966
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-16 17:47:58 +0300

    Good test for Hanoi Towers

--- HanoiTowers/HanoiTowers/HanoiTowers.cs
+++ HanoiTowers/HanoiTowers/HanoiTowers.cs
@@ -4,11 +4,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HanoiTowers
 {
     [TestClass]
-    public class UnitTest1
+    public class HanoiTowers
     {
         [TestMethod]
         public void TestMethod1()
         {
+            int[] source = new int[] { 4, 2, 1 };
+            int[] destination = new int[source.Length];
+            int[] auxiliar = new int[source.Length];
+            CollectionAssert.AreEqual(source, MoveTowers(3, source, destination, auxiliar));
+        }
+
+
+        int[] MoveTowers(int disk, int[] source, int[] destination, int[] auxiliar)
+        {
+            if (disk == 0)
+            {
+                destination[disk] = source[disk];
+                Array.Resize(ref source, source.Length - 1);
+                return destination;
+            }
+            else
+                MoveTowers(disk - 1, source, auxiliar, destination);
+                destination[disk-1] = source[disk-1];
+                Array.Resize(ref source, source.Length - 1);
+                MoveTowers(disk - 1, auxiliar, destination, source);
+            return destination;
         }
     }
 }
