ca6b8b4fbba1838a193311a80492d30d29da45e3
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 22:37:47 +0300

    PavementProblem moved here

---
+++ PavementProblem/PavementForMarket/PavementProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PavementForMarket
+{
+    [TestClass]
+    public class PavementProblem
+    {
+        [TestMethod]
+        public void FirstTestForExample()
+        {
+            int rocks = Pavement(6, 6, 4);
+            Assert.AreEqual(4, rocks);
+        }
+        [TestMethod]
+        public void SecondTestForMyValues()
+        {
+            int rocks = Pavement(10, 6, 4);
+            Assert.AreEqual(6, rocks);
+        }
+    
+        int Pavement(int mLength, int nWidth, int a)
+        {
+            int rocksPerLenght = (mLength / a);
+            int rocksPerWidth = (nWidth / a);
+       
+            if ((mLength%a) != 0 )
+            {
+                rocksPerLenght += 1 ;
+            }
+
+            if((nWidth%a) != 0)
+            {
+                rocksPerWidth += 1;
+            }
+
+            return rocksPerLenght * rocksPerWidth;
+        }
+    }
+}
