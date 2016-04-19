3e852e570b52a3a2e6097aed9e6516bc9024eeff
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 13:26:17 +0300

    Revert "Calculate First"
    This reverts commit 6b07566c0d30c7a20f2fb1e15f44c544111a4566.

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -9,26 +9,11 @@ namespace Calculator
         [TestMethod]
         public void TestMethod1()
         {
-            var calculate = "+ 1 1";
-            var instance = new BasicCalculator();
-            int param = 0;
-            Assert.AreEqual(2, instance.Calculate(calculate, ref param));
         }
     }
 
     public class BasicCalculator
     {
-        public double Calculate(string firstString, ref int param)
-        {
-            string[] elements = firstString.Split(' ');
-            double result;
 
-            if(double.TryParse(elements[param++], out result))
-            { 
-                return result;
-            }
-
-            return Calculate(firstString, ref param) + Calculate(firstString, ref param);
-        }
     }
 }
