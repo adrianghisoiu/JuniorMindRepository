e2fcda2271531d87495f2d2216f9b16fb3b0e7a8
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 13:07:08 +0300

    Add empty Calculator blank project

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public class BasicCalculator
+    {
+
+    }
+}
