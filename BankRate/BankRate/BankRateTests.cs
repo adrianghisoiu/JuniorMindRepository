2d385646624a2c6d5740c058eeedd8f516981131
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 14:55:03 +0300

    BankRate problem moved to this repository

---
+++ BankRate/BankRate/BankRateTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate
+{
+    [TestClass]
+    public class BankRateTests
+    {
+        [TestMethod]
+        public void RateForFirstMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 1);
+            Assert.AreEqual(102, rate);
+        }
+        [TestMethod]
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 2);
+            Assert.AreEqual(101, rate);
+        }
+        decimal CalculateBankRate(decimal total, int periodMonth, decimal interestPerYear, int currentMonth)
+        {
+            decimal principal = total / periodMonth;
+            decimal exactInterestPerMonth = interestPerYear / 12 / 100;
+            decimal sold = total - (currentMonth - 1) * principal;
+            return principal + sold * exactInterestPerMonth ;
+        }
+    }
+}
