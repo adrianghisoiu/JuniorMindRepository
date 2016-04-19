73e829d829c12c674772ef1df7c6523a963f3598
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 13:33:26 +0300

    First Test

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -7,13 +7,28 @@ namespace Calculator
     public class Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstTest()
         {
+            var calculate = "+ 1 1";
+            var instance = new BasicCalculator();
+            int param = 0;
+            Assert.AreEqual(2, instance.Calculate(calculate, ref param));
         }
     }
 
     public class BasicCalculator
     {
+        public double Calculate(string firstString, ref int param)
+        {
+            string[] elements = firstString.Split(' ');
+            double result;
 
+            if(double.TryParse(elements[param++], out result))
+            { 
+                return result;
+            }
+
+            return Calculate(firstString, ref param) + Calculate(firstString, ref param);
+        }
     }
 }
