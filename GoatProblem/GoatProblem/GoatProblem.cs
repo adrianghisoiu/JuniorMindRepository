9c8b7c144faac61441a087afdb0dd4427e5215fb
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 21:58:18 +0300

    Goat Problem

---
+++ GoatProblem/GoatProblem/GoatProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoatProblem
+{
+    [TestClass]
+    public class GoatProblem
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            int hayKilograms = CalculateKilogramsEatedByGoat(2, 2, 2, 4, 2);
+            Assert.AreEqual(1, hayKilograms);
+        }
+
+        int CalculateKilogramsEatedByGoat(int xDays, int yGoats, int zKilograms, int wDays, int qGoats)
+        {
+            return (xDays * qGoats * zKilograms) / (yGoats * wDays);
+            
+        }
+    }
+}
