c2991ff29dd7a9374e8c7e55c8d4e6c8437594a3
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-19 22:47:32 +0300

    SportProblem moved here

---
+++ SportProblem/SportProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportProblem
+{
+    [TestClass]
+    public class SportProblem
+    {
+        [TestMethod]
+        public void RepetitionForThreeRounds()
+        {
+            int total = CalculateNumberOfRepetetitions(9, 3);
+            Assert.AreEqual(9, total);           
+        }
+
+        int CalculateNumberOfRepetetitions(int totalRepetitions, int rounds)
+        {
+            totalRepetitions = rounds * rounds;
+            return totalRepetitions;
+        }
+    }
+}
