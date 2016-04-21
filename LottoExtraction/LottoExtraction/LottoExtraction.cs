92d880f6a851087bb026ad41aeea61f9f90fd08f
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-21 18:22:16 +0300

    Add empty LottoExtraction blank project

---
+++ LottoExtraction/LottoExtraction/LottoExtraction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoExtraction
+{
+    [TestClass]
+    public class LottoExtraction
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
