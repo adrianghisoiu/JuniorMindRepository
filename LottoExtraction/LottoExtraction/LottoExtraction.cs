ab84958f1f13b051dd2ef92c8ed63c530e9d1803
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-21 18:46:19 +0300

    Good test for Sorted Numbers

--- LottoExtraction/LottoExtraction/LottoExtraction.cs
+++ LottoExtraction/LottoExtraction/LottoExtraction.cs
@@ -7,8 +7,49 @@ namespace LottoExtraction
     public class LottoExtraction
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfNumbersAreSorted()
         {
+            Assert.IsTrue(AreSorted(GenerateNumbers(6)));
+        }
+
+        int[] GenerateNumbers(int totalNumber)
+        {
+            int[] generatedNumbers = new int[totalNumber];
+            Random random = new Random();
+            for (int i = 0; i < totalNumber; i++)
+            {
+                generatedNumbers[i] = random.Next(1, 49);
+               generatedNumbers = InsertionSort(generatedNumbers);
+            }
+
+            return generatedNumbers;
+        }
+
+        int[] InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+
+        bool AreSorted(int[] inputArray)
+        {
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i - 1] > inputArray[i])
+                    return false;
+            }
+            return true;
         }
     }
 }
