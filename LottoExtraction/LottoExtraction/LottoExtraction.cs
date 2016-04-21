c849d1844b9ec6dd7cb59af0086158a8f37271f6
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-04-21 19:23:24 +0300

    Lotto extraction modified

--- LottoExtraction/LottoExtraction/LottoExtraction.cs
+++ LottoExtraction/LottoExtraction/LottoExtraction.cs
@@ -33,13 +33,18 @@ namespace LottoExtraction
                 {
                     if (inputArray[j - 1] > inputArray[j])
                     {
-                        int temp = inputArray[j - 1];
-                        inputArray[j - 1] = inputArray[j];
-                        inputArray[j] = temp;
+                        Swap(ref inputArray[j-1], ref inputArray[j]);
                     }
                 }
             }
             return inputArray;
+        }
+
+        private static void Swap(ref int first, ref int second)
+        {
+            int temp =first;
+            first = second;
+            second = temp;
         }
 
         bool AreSorted(int[] inputArray)
