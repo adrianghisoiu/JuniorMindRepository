9a5400576e2e62ac49f5e987940fb9582298f93d
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-06-02 23:31:00 +0300

    Third good test for CompareWords

--- WordsSorting/WordsSorting/Word.Test.cs
+++ WordsSorting/WordsSorting/Word.Test.cs
@@ -8,10 +8,11 @@ namespace WordsSorting
         [Theory]
         [InlineData("a", "a", 0)]
         [InlineData("a", "b", -1)]
+        [InlineData("b", "a", 1)]
         public void CompareWord(string a, string b, int expected)
         {
-            var wordFirst = new Word(a, 1);
-            var wordSecond = new Word(b, 1);
+           /* var wordFirst = new Word(a, 1);
+            var wordSecond = new Word(b, 1);*/
             Assert.Equal(expected, a.CompareTo(b));
         }
     }
