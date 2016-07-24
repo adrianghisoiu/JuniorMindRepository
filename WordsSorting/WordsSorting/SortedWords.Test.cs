d28c77fa08873cca4b0c679d78ac6a75b6ace4e9
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-07-24 12:06:04 +0300

    Words sorted alphabetically

--- WordsSorting/WordsSorting/SortedWords.Test.cs
+++ WordsSorting/WordsSorting/SortedWords.Test.cs
@@ -18,5 +18,18 @@ namespace WordsSorting
             }
             Assert.Equal(new[] { new Word("b", 2), new Word("a", 1)}, words);
         }
+
+        [Fact]
+        public void SortByWord()
+        {
+            var expected = "b b a";
+            Words words = new Words();
+            string[] final = expected.Split(' ');
+            foreach (string w in final)
+            {
+                words.Add(w, "word");
+            }
+            Assert.Equal(new[] { new Word("a", 1), new Word( "b", 2) }, words);
+        }
     }
 }
