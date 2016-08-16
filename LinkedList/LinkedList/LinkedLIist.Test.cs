8a499a3d135eab46165a95fe9e1df7a5150df564
adrianghisoiu <ghisoiuiadrian@gmail.com>
2016-08-16 18:32:44 +0300

    AddLast and GetEnumerator solved

--- LinkedList/LinkedList/LinkedLIist.Test.cs
+++ LinkedList/LinkedList/LinkedLIist.Test.cs
@@ -8,18 +8,19 @@ namespace LinkedList
         [Fact]
         public void TestForAddFirst()
         {
-            var linkedList = new LinkedList<string> { };
-            linkedList.AddFirstItem("a");
-            Assert.Equal(new[] { "a" }, linkedList);
+            var linkedList = new LinkedList<string> {"a"};
+            linkedList.AddLast("c");
+            Assert.Equal(new[] { "a", "c" }, linkedList);
         }
 
         [Fact]
         public void TestForAddLast()
         {
             var linkedList = new LinkedList<string> { };
-            linkedList.AddFirstItem("a");
+            linkedList.AddLast("a");
             linkedList.AddLast("b");
-            Assert.Equal(new[] { "a", "b" }, linkedList);        
+            linkedList.AddLast("c");
+            Assert.Equal(new[] { "a", "b","c" }, linkedList);        
         }
     }
 }
--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -12,6 +12,7 @@ namespace LinkedList
         Node<T> head;
         int count;
 
+   
         public LinkedList()
         {
             head = new Node<T>(default(T));
@@ -20,32 +21,29 @@ namespace LinkedList
             this.count = 0;
         }
    
-        public void AddFirstItem(T item)
+        public void Add(T item)
         {
-            head = new Node<T>(item);
+            AddLast(item);
         }
 
         public void AddLast(T item)
         {
-            if (head == null)
-                this.AddFirstItem(item);
-            else
-            {
-                Node<T> newNode = new Node<T>(item);
-                head.Next = newNode;
-                newNode.Previous = head;
-                newNode.Next = head.Previous;
-            }
-             ++count;
+            Node<T> newNode = new Node<T>(item);
+
+            newNode.Next = head;
+            newNode.Previous = head.Previous;
+            head.Previous.Next = newNode;
+            head.Previous = newNode;
+            count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            var node = head;
-            while (node != null)
+            var node = head.Next;
+            while (node!=head)
             {
                 yield return node.Data;
-                node = node.Next;
+                node = node.Next;                   
             }
         }
 
